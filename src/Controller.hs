33a75ca4450ce802188d23a7bd2ae217a65848c2
Thomas <top-thomas@live.nl>
2019-10-25 17:15:12 +0200

    Pause bugfix and initial boardview

--- src/Controller.hs
+++ src/Controller.hs
@@ -8,19 +8,20 @@ import Graphics.Gloss.Interface.IO.Game
 
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
-step _ gstate@(GameState {pacman=(Pacman (x,y) d s), paused=Playing}) = case d of
+step _ gstate@(GameState {pacman=(Pacman (x,y) d s), paused=Paused}) = return gstate
+step _ gstate@(GameState {pacman=(Pacman (x,y) d s)}) = case d of
     N -> return gstate {pacman=Pacman (x,y+s) d s}
     O -> return gstate {pacman=Pacman (x+s,y) d s}
     Z -> return gstate {pacman=Pacman (x,y-s) d s}
     W -> return gstate {pacman=Pacman (x-s,y) d s}
-step _ gstate@(GameState {pacman=(Pacman (x,y) d s), paused=Paused}) = return gstate
+
 
 
 input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) Down _ _) gstate@(GameState {pacman=(Pacman (x,y) d s)})
+inputKey (EventKey (Char c) Down _ _) gstate@(GameState {pacman=(Pacman (x,y) d s), paused=Playing})
   = -- Pacman changes its direction based on key pressed
     case c of
     'a' -> gstate { pacman=Pacman (x,y) W s}
--- src/View.hs
+++ src/View.hs
@@ -9,11 +9,15 @@ view = return . viewPure
 viewPure :: GameState -> Picture
 viewPure gstate = pictures ((pacmanview gstate) : ((pauseview gstate) : ((ghostview gstate) ++ (boardview gstate)))) where
     pauseview :: GameState -> Picture
-    pauseview GameState {paused=p}
-        =
-            case p of
-            Paused -> translate (-200) (200) (color green (text ("Paused")))
-            Playing -> Blank
-    pacmanview GameState {pacman = (Pacman (x,y) _ _)} = translate x y (color yellow (circleSolid (9)))
+    pauseview GameState {paused=p} = case p of
+        Paused -> translate (-200) (200) (color green (text ("Paused")))
+        Playing -> Blank
+    pacmanview :: GameState -> Picture 
+    pacmanview GameState {pacman = (Pacman (x,y) _ _)} = translate x y (color yellow (circleSolid ((fromIntegral fieldsize) / 2 - 1 )))
     ghostview gstate = []
-    boardview gstate = []
+    boardview GameState {maze = m} = concat (map (map transform) m) where
+        transform f = case f of
+            M -> color blue (rectangleUpperWire 20 20)
+            D -> color white (circleSolid ((fromIntegral fieldsize) / 4))
+            L -> Blank 
+
