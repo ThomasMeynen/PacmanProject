7a3b4a869e5e4034083fcf310b2b43e4dde4b750
IetsCools420 <daan.renken@gmail.com>
2019-10-26 15:32:18 +0200

    het board werkt
    het board is nu hardcoded dit moet nog naar een losse file

--- src/View.hs
+++ src/View.hs
@@ -22,8 +22,8 @@ viewPure gstate = pictures ((pacmanview gstate) : ((pauseview gstate) : ((ghostv
             | xfields <= x = dots m 0 (y+1)
             | otherwise = [translate (transform x xfields) (transform y yfields) (field ((m!!y)!!x))] ++ dots m (x+1) y where
                 field :: Field -> Picture
-                field (M) = color blue (rectangleUpperWire 20 20)
-                field (D) = color white (circleSolid ((fromIntegral fieldsize) / 4))
+                field (M) = color blue (rectangleWire 20 20)
+                field (D) = color white (circleSolid ((fromIntegral fieldsize)/4))
                 field (L) = Blank 
                 transform :: Int -> Int -> Float
                 transform pos length = fromIntegral ((pos - (half length)) * fieldsize) :: Float
