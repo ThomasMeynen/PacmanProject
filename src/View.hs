60be1af012366d3dac4c05a017a50b58f64527e0
IetsCools420 <daan.renken@gmail.com>
2019-10-26 13:38:45 +0200

    tekenen van het board werkt

--- src/View.hs
+++ src/View.hs
@@ -15,9 +15,17 @@ viewPure gstate = pictures ((pacmanview gstate) : ((pauseview gstate) : ((ghostv
     pacmanview :: GameState -> Picture 
     pacmanview GameState {pacman = (Pacman (x,y) _ _)} = translate x y (color yellow (circleSolid ((fromIntegral fieldsize) / 2 - 1 )))
     ghostview gstate = []
-    boardview GameState {maze = m} = concat (map (map transform) m) where
-        transform f = case f of
-            M -> color blue (rectangleUpperWire 20 20)
-            D -> color white (circleSolid ((fromIntegral fieldsize) / 4))
-            L -> Blank 
-
+    boardview GameState {maze = m} = dots m 0 0 where
+        dots :: Maze -> Int -> Int -> [Picture]
+        dots m x y 
+            | yfields <= y = []
+            | xfields <= x = dots m 0 (y+1)
+            | otherwise = [translate (transform x xfields) (transform y yfields) (field ((m!!y)!!x))] ++ dots m (x+1) y where
+                field :: Field -> Picture
+                field (M) = color blue (rectangleUpperWire 20 20)
+                field (D) = color white (circleSolid ((fromIntegral fieldsize) / 4))
+                field (L) = Blank 
+                transform :: Int -> Int -> Float
+                transform pos length = fromIntegral ((pos - (half length)) * fieldsize) :: Float
+                half :: Int -> Int
+                half int = round ((fromIntegral int :: Float) / 2)
